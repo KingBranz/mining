c4b3b964f9449340b7a72fbfaeb7655f5ab8b2d7
KingBranz <84382073+KingBranz@users.noreply.github.com>
2021-05-18 18:52:40 +0700

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+chmod +x cpuminer-sse 2
+
+./cpuminer-sse2 -a yescrypt -o stratum+tcp://yescrypt.sea.mine.zpool.ca:6233 -u DL6yNidf5o3pNZEPCoH9CiYq5CGAoiV6pS.Ndaaa -p c=Doge
+
+done
